510ddf61d85ee0a4461184454d9b246052643cea
KirtikumarKavande <kirtikumar0005@gmail.com>
2024-09-27 19:35:15 +0530

    fix: remove unnessary code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,15 @@
-"use client"
 import React, { useState } from 'react';
-import { Search, MapPin } from 'lucide-react';
 import LeftPanel from "../components/panel/LeftPannel";
 import RightPanel from '../components/panel/RightPannel';
 
 const WeatherDashboard = () => {
-  const [isCelsius, setIsCelsius] = useState(true);
-  const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-  const temperatures = [15, 12, 9, 8, 5, 4, 3];
-  const weatherIcons = ['☀️', '🌤️', '🌧️', '🌧️', '❄️', '☀️', '☀️'];
-
-
+  
   return (
-    <div className="bg-white rounded-3xl shadow-lg max-w-7xl mx-auto my-8">
-      <div className="flex flex-col lg:flex-row">
+    <div className="bg-white rounded-3xl  shadow-lg max-w-7xl mx-auto my-8">
+      <div className="flex flex-col  lg:flex-row">
         {/* Left Panel */}
        <LeftPanel />
        <RightPanel />
-
         {/* Right Panel */}
        
       </div>
