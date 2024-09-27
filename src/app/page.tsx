19db3ee56db5af5a3297fc956b33d7f513076ff5
KirtikumarKavande <kirtikumar0005@gmail.com>
2024-09-27 20:08:06 +0530

    feature: create hook for fetch data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,20 @@
-import React, { useState } from 'react';
+"use client";
+import React, { useEffect, useState } from 'react';
 import LeftPanel from "../components/panel/LeftPannel";
 import RightPanel from '../components/panel/RightPannel';
+import useFetch from "../hooks/useFetch"
 
 const WeatherDashboard = () => {
-  
+
+  const fetchData=useFetch()
+  useEffect(()=>{
+   async function abc(){
+      const data=await fetchData("v1","forecast.json?q=london&days=7&aqi=yes")
+      console.log(data)
+    }
+    abc()
+   
+  },[])
   return (
     <div className="bg-white rounded-3xl  shadow-lg max-w-7xl mx-auto my-8">
       <div className="flex flex-col  lg:flex-row">
---
+++ src/hooks/useFetch.tsx
@@ -0,0 +1,19 @@
+"use client"
+const useFetch = () => {
+
+    const getDataFromDB = async (apiVersion: string ,url:string) => {
+        try {
+            const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/${apiVersion}/${url}&key=${process.env.NEXT_PUBLIC_WEATHER_API_KEY}`, {
+                method: "GET",
+            });
+            const data = await res.json();
+
+            return data;
+        } catch (err) {
+            return { statusCode: 400, message: "something went wrong" };
+        }
+    };
+    return getDataFromDB;
+};
+
+export default useFetch;
